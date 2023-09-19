097abb9747106ce1df536724a86c62f958b1482f
RashidAvl <rashidmohdashraf94@gmail.com>
2023-09-19 19:53:14 +0530

    change test

--- deploytest/src/App.js
+++ deploytest/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Apps-header">
         <img src={logo} className="App-logo" alt="logo" />
        <h1>this is new testing</h1>
       </header>
